30df9d66782698d91ecca0757da8c1bb4db9e1c3
anisulislam10 <anis.inbox10@gmail.com>
2025-09-16 21:30:17 +0500

    new changes

--- pages/api/build.js
+++ pages/api/build.js
@@ -7,6 +7,9 @@ export default async (req, res) => {
   const buildDetails = await BuildCodesArray(build);
   const comments = buildDetails.join();
   let sendEmailToClient = false;
+    let sendEmailToowner = false;
+    let sendEmailToadmin = false;
+
   const products = buildDetails.map((entry) => entry.split(" ")[0]);
 
   const clientData = {
@@ -21,12 +24,40 @@ export default async (req, res) => {
     products: products,
     type: "builder",
   };
+   const clientDataowner = {
+    first_name: firstName,
+    last_name: lastName,
+    company_name: company,
+    email: 'anisuldev@gmail.com',
+    phone_number: phone,
+    source: source,
+    lead_sdr: leadSrc,
+    statement: comments,
+    products: products,
+    type: "builder",
+  };
+ const clientDataadmin = {
+    first_name: firstName,
+    last_name: lastName,
+    company_name: company,
+    email: 'info@request.atgpharma.com',
+    phone_number: phone,
+    source: source,
+    lead_sdr: leadSrc,
+    statement: comments,
+    products: products,
+    type: "builder",
+  };
 
   body.lat && (clientData.latitude = body.lat);
   body.long && (clientData.longitude = body.long);
 
   try {
     sendEmailToClient = await sendEmailMsg(clientData);
+    sendEmailToowner = await sendEmailMsgOwner(clientDataowner);
+        sendEmailToadmin = await sendEmailMsgadmin(clientDataadmin);
+
+
   } catch (err) {
     console.log("error", err);
   }
@@ -53,6 +84,62 @@ const BuildCodesArray = (build) => {
       resolve(fillStringArr);
     } else {
       reject("Did not work");
+    }
+  });
+};
+const sendEmailMsgOwner = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const request = await axios({
+        url: "https://atg-connect-api.herokuapp.com/atgWeb/quote/",
+        method: "post",
+        data: data,
+        headers: {
+          "X-CSRFToken":
+            "JvrtqvaC9mWJdfavKMHrdWp9ZJrY4diiynLqD6j7hQOXbEYAcEdutIkqpjykum34",
+          "content-type": "application/json",
+          accept: "application/json",
+        },
+      });
+      if (request.status === 200) {
+        resolve(true);  
+      } else {
+        
+        resolve(false);
+        console.log("Error response: ", request);
+      }
+    } catch (error) {
+      console.log("error response", error.toJSON());
+      // console.log("error data:", error.data);
+      resolve(false);
+    }
+  });
+};
+const sendEmailMsgadmin = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const request = await axios({
+        url: "https://atg-connect-api.herokuapp.com/atgWeb/quote/",
+        method: "post",
+        data: data,
+        headers: {
+          "X-CSRFToken":
+            "JvrtqvaC9mWJdfavKMHrdWp9ZJrY4diiynLqD6j7hQOXbEYAcEdutIkqpjykum34",
+          "content-type": "application/json",
+          accept: "application/json",
+        },
+      });
+      if (request.status === 200) {
+        resolve(true);
+      } else {
+        
+        resolve(false);
+        console.log("Error response: ", request);
+      }
+    } catch (error) {
+      console.log("error response", error.toJSON());
+      // console.log("error data:", error.data);
+      resolve(false);
     }
   });
 };
